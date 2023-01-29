c2d49d89f1b8ae6ccdee07033501e0e89edfe8d1
hussein <hussiahmed.ahmed@gmail.com>
2023-01-29 18:02:17 -0500

    initial commit

---
+++ helper_functions.py
@@ -0,0 +1,215 @@
+import pandas as pd
+import numpy as np
+import re
+from datetime import datetime, timedelta, time
+
+def transform_schedule(file_name, sheet_name):
+    """ This function transforms the user input schedule into a format that can be further analyzed
+    
+    parameters: 
+    file_name: excel or xlsx file format
+    sheet_name: name of the sheet as in excel file
+
+    returns:
+    A dataframe that contains four columns, employee name, date, hour in, hour out
+
+    """
+    #read excel file
+    df = pd.read_excel(file_name, sheet_name= sheet_name)
+
+    #remove first row which usually contains the name of the building for ex. hamilton cove
+    df = df.iloc[1:]
+
+    #replace null values with a string called unknown, this will help us down the road
+    df= df.replace(np.NaN, "Unknown").reset_index(drop=True).iloc[:,1:].reset_index(drop=True)
+
+    #find the index of the first week, from here you will determine where the first week starts 
+    index_of_first_week = df[df['Unnamed: 1'].str.contains("Date") == True].index[0]
+    index_of_second_week = df[df['Unnamed: 1'].str.contains("Date") == True].index[1]
+
+    #rename columns of the dataframe to match that the row of where the first week starts, the columns are now dates that start from 1/16 to 1/22 (first week only)
+    df.columns = df.iloc[index_of_first_week]
+    df = df.iloc[:index_of_second_week, : ]
+
+    #lets drop any unknown value in the Date column, if there are no employees why keep it?
+    #The date column is just called DATE but in reality that column contains names of each employee
+    df = df[df['Date'] !="Unknown"].reset_index(drop =True)
+    df = df[df['Date'] !="Name of Employee"].reset_index(drop =True)
+    df = df[df['Date'] !="Date"].reset_index(drop =True)
+    
+    #unpivot the date while fixing the name of the employee
+    melt_df = pd.melt(df, id_vars="Date")
+    
+    #clean up the melt dataframe by removing any employee without work hours, and remove any text like the word "day"
+    melt_df = melt_df[melt_df['value'] !="Unknown"].reset_index(drop =True)
+    melt_df = melt_df[melt_df['Date'] !="Day"].reset_index(drop =True)
+
+    #some rows that contain total hours get pulled in, but we only need the time worked, our strategy to keep only hourly date was to focus on strings with the : format
+    melt_df['value'] = melt_df['value'].astype("str")
+    melt_df = melt_df[melt_df['value'].str.contains(":")]
+
+    #rename some of the columns
+    melt_df = melt_df.rename(columns={"Date":"Employee Name", "value":"Hours"})
+    melt_df.columns.values[1] = "Date"
+
+    #we found an interesting pattern, if the Date had an unknown string, that meant that the this is the time out, but if they had a time instead, that meant time in. 
+    # so we split the data into test_df1 and test_df2 that we could then merge
+    df1 = melt_df[melt_df['Date'] != "Unknown"].reset_index(drop=True)
+    df1 = df1.rename(columns={"Hours":"Time In"})
+    
+    df2 = melt_df[melt_df['Date'] == "Unknown"].reset_index(drop=True)
+    df2 = df2.rename(columns={"Hours":"Time Out"})
+    df2 = df2[["Time Out"]]
+    
+    merged_df = pd.merge(left=df1, right=df2, left_index=True, right_index=True)
+
+    return merged_df
+
+
+def hours_worked(merged_test_df):
+    """Calculates the number of hours worked by each employee in the schedule
+    parameters:
+    merged_test_df: a transformed dataframe
+
+    returns:
+    an additional column that computes the hours worked of each employee
+    """
+
+
+    #make sure that time in and out are in datetime formats
+    merged_test_df['Time In'] = pd.to_datetime(merged_test_df['Time In'], format='%H:%M:%S')
+    merged_test_df['Time Out'] = pd.to_datetime(merged_test_df['Time Out'], format='%H:%M:%S')
+
+    #calculate the number of hours worked between time in and time out
+    merged_test_df['Hours Worked'] = merged_test_df['Time Out'] - merged_test_df['Time In']
+    merged_test_df['Hours Worked'] = merged_test_df['Hours Worked'].apply(lambda x: x.total_seconds() / 3600 if x.total_seconds() > 0 else (x + pd.Timedelta('1D')).total_seconds() / 3600)
+    return merged_test_df
+
+
+# def get_building_name(file_name, sheet_name):
+#     """ Uses Regex to extract the name of the building"""
+#     df = pd.read_excel(file_name, sheet_name= sheet_name)
+#     text = df.iloc[:1]['Unnamed: 1'][0]
+    
+#     match = re.search(r'^(\w+)\s(Weekly|employees)', text, re.IGNORECASE)
+#     if match:
+#         x = match.group(1)
+#     else:
+#         match = re.search(r'^(\d+\s\w+)\s(Weekly|employees)', text, re.IGNORECASE)
+#         if match:
+#             x = match.group(1)
+#         else:
+#             x = None
+
+#     return x
+
+def get_building_name(file_name, sheet_name):
+    """ Uses Regex to extract the name of the building"""
+    df = pd.read_excel(file_name, sheet_name= sheet_name)
+    text = df.iloc[:1]['Unnamed: 1'][0]
+    
+    match = re.search(r'^([\w\s]+)\s(Weekly|employees)', text, re.IGNORECASE)
+    if match:
+        x = match.group(1)
+    else:
+        x = None
+
+    return x
+
+
+
+def process_hours(df):
+    """
+    Takes a dataframe and categorizes the total hours by each employee into three categories (holliday, regular and overtime hours)
+
+    """
+
+    # Create a new DataFrame with the desired columns
+    result = pd.DataFrame(columns=['Employee Name', 'Week of Year', 'Holiday Hours', 'Regular Hours', 'Overtime Hours'])
+
+    df['Employee Name'] = df['Employee Name'].str.strip() #removes any white spaces that may be created by mistake
+    
+    # Group the data by employee and week of year
+    grouped = df.groupby(['Employee Name', 'Week of year'])
+    
+    # Iterate over each group and calculate the hours
+    for name, group in grouped:
+        holiday_hours = group[group['holiday'] == 1]['Hours Worked'].sum()
+        regular_hours = group[group['holiday'] == 0]['Hours Worked'].sum()
+        overtime_hours = 0
+        if regular_hours > 40:
+            overtime_hours = regular_hours - 40
+            regular_hours = 40
+            
+            
+        result = result.append({'Employee Name': name[0], 'Week of Year': name[1], 'Holiday Hours': holiday_hours, 'Regular Hours': regular_hours, 'Overtime Hours': overtime_hours}, ignore_index=True)
+
+    return result
+
+def convert_df(df):
+    df = df.sort_values(by="Employee Name", ascending=True)
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
+
+def transform_schedule_week2(file_name, sheet_name):
+    """ This function transforms the user input schedule into a format that can be further analyzed
+    
+    parameters: 
+    file_name: excel or xlsx file format
+    sheet_name: name of the sheet as in excel file
+
+    returns:
+    A dataframe that contains four columns, employee name, date, hour in, hour out
+
+    """
+    #read excel file
+    df = pd.read_excel(file_name, sheet_name= sheet_name)
+
+    #remove first row which usually contains the name of the building for ex. hamilton cove
+    df = df.iloc[1:]
+
+    #replace null values with a string called unknown, this will help us down the road
+    df= df.replace(np.NaN, "Unknown").reset_index(drop=True).iloc[:,1:].reset_index(drop=True)
+
+    # #find the index of the first week, from here you will determine where the first week starts 
+    index_of_first_week = df[df['Unnamed: 1'].str.contains("Date") == True].index[0]
+    index_of_second_week = df[df['Unnamed: 1'].str.contains("Date") == True].index[1]
+
+    # #rename columns of the dataframe to match that the row of where the first week starts, the columns are now dates that start from 1/16 to 1/22 (first week only)
+    df.columns = df.iloc[index_of_second_week]
+    df = df.iloc[index_of_second_week:, : ].reset_index(drop=True)
+
+    #lets drop any unknown value in the Date column, if there are no employees why keep it?
+    #The date column is just called DATE but in reality that column contains names of each employee
+    df = df[df['Date'] !="Unknown"].reset_index(drop =True)
+    df = df[df['Date'] !="Name of Employee"].reset_index(drop =True)
+    df = df[df['Date'] !="Date"].reset_index(drop =True)
+    
+    #unpivot the date while fixing the name of the employee
+    melt_df = pd.melt(df, id_vars="Date")
+    
+    #clean up the melt dataframe by removing any employee without work hours, and remove any text like the word "day"
+    melt_df = melt_df[melt_df['value'] !="Unknown"].reset_index(drop =True)
+    melt_df = melt_df[melt_df['Date'] !="Day"].reset_index(drop =True)
+
+    #some rows that contain total hours get pulled in, but we only need the time worked, our strategy to keep only hourly date was to focus on strings with the : format
+    melt_df['value'] = melt_df['value'].astype("str")
+    melt_df = melt_df[melt_df['value'].str.contains(":")]
+
+    #rename some of the columns
+    melt_df = melt_df.rename(columns={"Date":"Employee Name", "value":"Hours"})
+    melt_df.columns.values[1] = "Date"
+  
+
+    # we found an interesting pattern, if the Date had an unknown string, that meant that the this is the time out, but if they had a time instead, that meant time in. 
+    # so we split the data into test_df1 and test_df2 that we could then merge
+    df1 = melt_df[melt_df['Date'] != "Unknown"].reset_index(drop=True)
+    df1 = df1.rename(columns={"Hours":"Time In"})
+    
+    df2 = melt_df[melt_df['Date'] == "Unknown"].reset_index(drop=True)
+    df2 = df2.rename(columns={"Hours":"Time Out"})
+    df2 = df2[["Time Out"]]
+    
+    merged_df = pd.merge(left=df1, right=df2, left_index=True, right_index=True)
+
+    return   merged_df
---
+++ super_app.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+from datetime import datetime, timedelta, time
+import re
+import helper_functions as x
+import altair as alt
+
+
+st.title(""" How It Works 	:grey_question:""")
+st.caption("""
+To get started, simply upload files/schedules in excel format. 
+""")
+
+with st.sidebar:
+    st.sidebar.image("logo_transparent.png")
+
+# Create a list of holiday dates
+holidays = ['2023-11-23', '2023-11-25', '2023-12-31', '2023-07-04', '2023-05-29', '2023-09-04']
+df_list = [] #acts as a database
+df_list_week_two = []
+
+
+with st.form(key='my_form'):
+    st.subheader("1- Upload Time Sheets")
+
+    files_names = st.file_uploader("📗 Choose an excel file to upload", accept_multiple_files = True, help="You can upload multiple files at once")
+    st.write("                                                       ")
+    st.subheader("2- Provide sheet name ")
+    sheet_name = st.text_input("📄 example: 1-16 to 1-29", help="Make sure it matches exactly your tab name in the excel file")
+
+    st.write("""                                                                         """)
+    st.subheader("3-  Click Submit")
+    submit_button = st.form_submit_button(label='✅ Submit')
+    if submit_button:
+        for file in files_names: #iterate through the user uploaded files
+            transformed_file = x.transform_schedule(file, sheet_name=sheet_name) #apply transformation function to each file that will calculate the hours worked
+            transformed_file_week2 = x.transform_schedule_week2(file, sheet_name=sheet_name)
+
+            building_name = x.get_building_name(file, sheet_name) #bring in the name of the building
+            transformed_file['Building Name'] = building_name #add a building name column to the dataframe
+            transformed_file_week2['Building Name'] = building_name
+            df_list.append(transformed_file) #save returned dataframe to a list named df_list
+            df_list_week_two.append(transformed_file_week2)
+
+    else:
+        st.caption(" No files uploaded")
+
+try: 
+    df_week_one = pd.concat(df_list, axis=0) #stack all files together
+    df_week_two = pd.concat(df_list_week_two, axis=0)
+    df = pd.concat([df_week_one, df_week_two], axis=0)
+
+    df['holiday'] = df['Date'].apply(lambda x: 1 if x.date().isoformat() in holidays else 0) #adds a column that has the holidays in it
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['Week of year'] = df['Date'].apply(lambda x: x.isocalendar()[1])
+    df = x.hours_worked(df) #apply the hours worked function to determine by employee
+    final_df = x.process_hours(df)
+    grouped_final_view = final_df.groupby("Employee Name").agg({"Holiday Hours":"sum", "Regular Hours":"sum", "Overtime Hours":"sum"})
+
+    st.dataframe(grouped_final_view.style.format({'Holiday Hours': '{:,.1f}', 'Regular Hours': '{:,.1f}', 'Overtime Hours': '{:,.1f}'}), width=1000) #render result on streamlit 
+
+    prcs_csv = x.convert_df(grouped_final_view)
+    st.download_button(
+        label=":arrow_down: Download to excel",
+        data=prcs_csv,
+        file_name='auto-report.csv',
+        mime='text/csv',)
+   
+    st.write("                                      ")
+    st.markdown("### Summary of hours worked by building")
+
+    visual_df = df.groupby(["Building Name"]).agg({"Hours Worked":"sum"}).reset_index()
+
+    c = alt.Chart(visual_df).mark_bar().encode(x = "Hours Worked", y="Building Name")
+    c_text = c.mark_text(align="left", baseline="middle", dx=3).encode(text="Hours Worked:Q")
+    c_figure = (c + c_text).properties(height=300, width=900)
+    st.altair_chart(c_figure)
+
+    #checks and balances 
+    #week one dates uploaded and processed
+
+except:
+    st.warning("Please upload a file before proceeding.")
+    
+
+
+    
